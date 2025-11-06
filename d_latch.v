1cad30fb08b9d7a58512f31abda3396457f4404e
Irma S. <156287214+irmasmajlovic@users.noreply.github.com>
2025-11-06 16:48:09 -0700

    Update d_latch.v

--- d_latch.v
+++ d_latch.v
@@ -1,1 +1,11 @@
-
+module d_latch(
+    input D, E,
+    output reg Q,
+    output notQ
+    );
+    always @(D, E) begin
+        if(E)
+            Q <= D;
+    end
+    assign notQ = ~Q;           
+endmodule
