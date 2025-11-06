46eb93b9aa5cb7f9003966ac503e3eaac59e1f50
Irma S. <156287214+irmasmajlovic@users.noreply.github.com>
2025-11-06 16:50:09 -0700

    Update mux.v

--- mux.v
+++ mux.v
@@ -1,1 +1,10 @@
-
+module mux(
+    input [1:0] Sel,
+    input [7:0] A,
+    input [7:0] B,
+    input [7:0] C,
+    input [7:0] D,
+    output [7:0] data
+);
+assign data = (~Sel[0] & ~Sel[1])? A: (Sel[0] & ~Sel[1])? B: (~Sel[0] &  Sel[1])? C : (Sel[0] &  Sel[1])? D:0000;
+endmodule
