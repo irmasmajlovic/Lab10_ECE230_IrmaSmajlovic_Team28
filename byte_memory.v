61407ab58e34e65ef603e68685d245716be11c79
Irma S. <156287214+irmasmajlovic@users.noreply.github.com>
2025-11-06 16:39:13 -0700

    Update byte_memory.v

--- byte_memory.v
+++ byte_memory.v
@@ -1,1 +1,10 @@
-
+module byte_memory(
+    input [7:0] data,
+    input store,
+    output reg [7:0] memory
+    );
+    always @(data, store) begin
+        if(store)
+            memory <= data;
+    end        
+endmodule
